7d2b1f591fde8b48b5cdb543a08139d47ed2648b
muxiaolei <1484542698@qq.com>
2018-06-14 14:38:55 +0800

    添加TODO

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ App({
 		userInfo: null,
     //服务网点id
     server_id: '0', 
-    //baseUrl: 'http://192.168.1.114:8000/',
-    //baseUrl: 'https://wxapi.benpaobao.com/',
-    baseUrl: 'https://wxapi2.benpaobao.com/',
+    //baseUrl: 'http://192.168.1.141:8000/',
+    baseUrl:'https://wxapi.benpaobao.com/',
+    //baseUrl: 'https://wxapi2.benpaobao.com/',
 	}
 })
